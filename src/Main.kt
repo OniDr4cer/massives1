c396137d0138dff405dd9522ec871775dab7c454
пылесос <zava@weqew.sdsa>
2025-09-15 13:05:42 +0300

    new task

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    print("Введите первое число: ")
+    val chast01 = readln()
+    print("Введите второе число: ")
+    val chast02 = readln()
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    val chetNechet = false
+
+    val chast01All = chast01 + chast02
+    if (chast01All.toInt() % 2 == 1){
+        println("Вывод нечетного числа - $chast01All")
+        val chetNechet = true
+    }
+    val chast02All = chast02 + chast01
+    if (chast02All.toInt() % 2 == 1){
+        println("Вывод нечетного числа - $chast02All")
+        val chetNechet = true
+    }
+
+    if(!chetNechet) {
+        println("Вывести нечетное число невозможно")
     }
 }
